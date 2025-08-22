8657bed659c2a95151910362b1f440430d889679
Paulo <paulovitor.lavinas@gmail.com>
2025-08-22 17:06:23 -0300

    feat: cria o modelo para filmes e streaming, adiciona coreapps no installed_apps e atualiza o dockerfile para a instalação do PostgreSQL

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Filmes(models.Model):
+    titulo = models.CharField(max_length=255)
+    sinopse = models.TextField()
+    capa = models.URLField()
+    id_tmdb = models.IntegerField()
+
+class Streaming(models.Model):
+    nome = models.CharField(max_length=100)
+    url = models.URLField()
