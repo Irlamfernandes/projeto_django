6370e3571d58246ba6bad844fcd7e46e1644da67
emanuelrf7 <emanuelrf8@gmail.com>
2025-08-26 19:24:46 -0300

    Include coreapp.urls into core urls.py file

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from coreapp import views
 
 urlpatterns = [
+    path('', include('coreapp.urls')),
     path('admin/', admin.site.urls),
     path('ola/', views.ola),
     path('', views.ola),
