561f46987b0b7d481b379310a9a2603c77d28217
Irlam Fernandes <lanzinhofernandes@gmail.com>
2025-09-18 21:34:13 -0300

    fix: ajustando a rota da views

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.buscar_filme, name = 'buscar_filme'),
+    path("buscar/", views.BuscarFilmeView.as_view(), name = 'buscar_filme'),
 ]
