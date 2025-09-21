85f684089e7ab9eff0452f07e8df14e053028e89
Paulo <paulovitor.lavinas@gmail.com>
2025-09-21 16:34:05 -0300

    feat: creating URL for index

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('resultados/', TemplateView.as_view(template_name='coreapp/resultados.html'), name='resultados'),
+    path('index/', TemplateView.as_view(template_name='coreapp/index.html'), name='index'),
 ]
